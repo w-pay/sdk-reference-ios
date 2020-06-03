bfd3c5491321c8cf6905a4555f154e483718d134
Kieran Simpson <kieran@redcrew.com.au>
2020-06-03 14:45:01 +1000

    Project init

---
+++ VillageWalletClientSDKReference/PaymentConfirmViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class PaymentConfirmViewController: UIViewController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+}
+
