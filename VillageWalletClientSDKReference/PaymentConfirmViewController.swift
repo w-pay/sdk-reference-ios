cb86638ba559d67e411f9865209395c517ca102d
Kieran Simpson <kieran@redcrew.com.au>
2020-06-04 13:32:40 +1000

    Add bottom sheet.

--- VillageWalletClientSDKReference/PaymentConfirmViewController.swift
+++ VillageWalletClientSDKReference/PaymentConfirmViewController.swift
@@ -5,7 +5,6 @@ class PaymentConfirmViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
 	}
 
 	@IBAction func makePayment(_ sender: UIButton) {
