170efbdba5e8e06cf974286fdfadebd19186b68e
Kieran Simpson <kieran@redcrew.com.au>
2020-06-04 12:42:52 +1000

    First draft of UI.
    Adds basic layout and properties of UI
    Adds navigation

--- VillageWalletClientSDKReference/PaymentConfirmViewController.swift
+++ VillageWalletClientSDKReference/PaymentConfirmViewController.swift
@@ -1,9 +1,23 @@
 import UIKit
 
 class PaymentConfirmViewController: UIViewController {
+	@IBOutlet weak var action: UILabel!
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+	}
+
+	@IBAction func makePayment(_ sender: UIButton) {
+		action.text = "Paying"
+
+		setTimeout(2) {
+			self.performSegue(withIdentifier: "ShowReceipt", sender: self)
+		}
+	}
+
+	private func setTimeout(_ delay: TimeInterval, block: @escaping () -> Void) -> Timer {
+		return Timer.scheduledTimer(timeInterval: delay, target: BlockOperation(block: block), selector: #selector(Operation.main), userInfo: nil, repeats: false)
 	}
 }
 
