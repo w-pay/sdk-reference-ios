98db50c4f5fadd04ff663e86bbaf54b71deee7e6
Kieran Simpson <kieran@redcrew.com.au>
2020-06-15 17:01:48 +1000

    Add PaymentService

---
+++ VillageWalletClientSDKReference/PaymentService.swift
@@ -0,0 +1,44 @@
+import UIKit
+import OpenAPIClient
+
+class PaymentService {
+	let apiClient: OAIApiClient
+	let api: OAICustomerApi
+
+	init() {
+		let config = OAIDefaultConfiguration()
+		config.accessToken = "ODA4NTYyNDktNjg0Ny00OWY4LWFmMDItOTU1MWEwMzliMjg5OlZJTExBR0VfQ1VTVE9NRVI="
+
+		apiClient = OAIApiClient(
+			baseURL: URL(string: "https://woolies-village.appspot.com"),
+			configuration: config
+		)
+
+		api = OAICustomerApi(apiClient: apiClient)
+	}
+
+	func retrievePaymentRequestDetails(qrCodeId: String, callback: @escaping (OAICustomerPaymentDetail?, HTTPURLResponse?) -> Void) {
+		api.getCustomerPaymentDetailsByQRCodeId(withQrId: qrCodeId, completionHandler: { result, error in
+			guard error == nil else {
+				let resp = self.extractHttpResponse(error: error! as NSError)
+
+				callback(nil, resp)
+
+				return
+			}
+
+			callback(result!.data, nil)
+		})
+	}
+
+	private func extractHttpResponse(error: NSError) -> HTTPURLResponse {
+		let info = (error as NSError).userInfo
+		let data = info["com.alamofire.serialization.response.error.response"] as? HTTPURLResponse
+
+		guard data != nil else {
+			fatalError("No HTTP response data in error")
+		}
+
+		return data!
+	}
+}
